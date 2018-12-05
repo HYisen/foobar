02bac759c82b791b01bf4c9e8affddd2dc12f781
Alex <alexhyisen@gmail.com>
2018-12-05 20:41:34 +0800

    fix some trivial problems in descriptions of DB design

--- src/main/java/net/alexhyisen/foobar/module/Greeting.java
+++ src/main/java/net/alexhyisen/foobar/module/Greeting.java
@@ -1,5 +1,8 @@
 package net.alexhyisen.foobar.module;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Greeting {
     private final long count;
     private final String msg;
@@ -15,5 +18,10 @@ public class Greeting {
     public Greeting(long count, String msg) {
         this.count = count;
         this.msg = msg;
+    }
+
+    public static void main(String[] args) {
+        var one = LocalDateTime.now().minus(18, ChronoUnit.HOURS);
+        System.out.print(one.toString());
     }
 }
