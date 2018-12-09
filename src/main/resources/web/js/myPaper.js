4f7e26fe1fcade96ccb1e5580c7954f1d9088f48
fishtree <1269605928@qq.com>
2018-12-09 20:17:11 +0800

    moments move to myPaper moments update

---
+++ src/main/resources/web/js/myPaper.js
@@ -0,0 +1,28 @@
+function showMyPaper()
+{
+    console.log("BANG!");
+
+    let ajaxGet = (url, callback) => {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                callback(xmlhttp.responseText);
+
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    };
+
+    ajaxGet("http://localhost:8080/10000/paper",(text)=>{
+        console.log(text);
+        let json = JSON.parse(text);
+        for (let i in json) {
+            document.getElementById("time_"+i).innerText = json[i].timestamp;
+            document.getElementById("nickname_"+i).innerText = json[i].person.nickname;
+            document.getElementById("title_"+i).innerText = json[i].paper.title;
+            document.getElementById("content_"+i).innerHTML = json[i].paper.content;
+        }
+    })
+}
+
