690bacd49f7de6949937b3084235311ac6770450
fishtree <1269605928@qq.com>
2018-12-10 23:15:17 +0800

    moments update: load first batch of date to fill the window. load more data when scroll bottom  of the window or window size changed. refresh the data every minutes or click the button.

--- src/main/resources/web/js/moments.js
+++ src/main/resources/web/js/moments.js
@@ -1,5 +1,54 @@
-function showMoments()
-{
+function showMoments() {
+    pages --;
+    console.log("BANG!");
+
+    let ajaxGet = (url, callback) => {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                callback(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    };
+
+    ajaxGet("http://localhost:8080/10002/moment?limit=" + pages * itemInPages, (text) => {
+            console.log(text);
+            let json = JSON.parse(text);
+            let oldContainer = document.getElementById("momentContainer");
+            let container = document.createElement("p");
+            container.id = "momentContainer";
+            let body = oldContainer.parentElement;
+            body.removeChild(oldContainer)
+            body.append(container);
+            for (let i in json) {
+                let oneContainer = document.createElement("table");
+                let titleTR = document.createElement("tr");
+                let temp = document.createElement("td");
+                temp.innerText = json[i].timestamp;
+                titleTR.append(temp);
+                temp = document.createElement("td");
+                temp.innerText = json[i].person.nickname;
+                titleTR.append(temp);
+                temp = document.createElement("td");
+                temp.innerText = json[i].paper.title;
+                titleTR.append(temp);
+                let contantTR = document.createElement("tr");
+                temp = document.createElement("td");
+                temp.innerText = json[i].paper.content;
+                contantTR.append(temp);
+                oneContainer.append(titleTR);
+                oneContainer.append(contantTR);
+                container.append(oneContainer);
+            }
+            end = false;
+            loadMoreMoments()
+        }
+    )
+}
+
+function loadMoreMoments() {
     console.log("BANG!");
 
     let ajaxGet = (url, callback) => {
@@ -14,15 +63,42 @@ function showMoments()
         xmlhttp.send();
     };
 
-    ajaxGet("http://localhost:8080/10000/moment",(text)=>{
-        console.log(text);
-        let json = JSON.parse(text);
-        for (let i in json) {
-            document.getElementById("time_"+i).innerText = json[i].timestamp;
-            document.getElementById("nickname_"+i).innerText = json[i].person.nickname;
-            document.getElementById("title_"+i).innerText = json[i].paper.title;
-            document.getElementById("content_"+i).innerHTML = json[i].paper.content;
+    ajaxGet("http://localhost:8080/10002/moment?skip=" + pages * itemInPages + "&limit=" + itemInPages, (text) => {
+            console.log(text);
+            let json = JSON.parse(text);
+            let container = document.getElementById("momentContainer");
+            if (json.length == 0) {
+                let endContainer = document.createElement("p");
+                endContainer.innerText = "没有更早的信息了~";
+                end = true;
+                container.appendChild(endContainer);
+            }
+            for (let i in json) {
+                let oneContainer = document.createElement("table");
+                let titleTR = document.createElement("tr");
+                let temp = document.createElement("td");
+                temp.innerText = json[i].timestamp;
+                titleTR.append(temp);
+                temp = document.createElement("td");
+                temp.innerText = json[i].person.nickname;
+                titleTR.append(temp);
+                temp = document.createElement("td");
+                temp.innerText = json[i].paper.title;
+                titleTR.append(temp);
+                let contantTR = document.createElement("tr");
+                temp = document.createElement("td");
+                temp.innerText = json[i].paper.content;
+                contantTR.append(temp);
+                oneContainer.append(titleTR);
+                oneContainer.append(contantTR);
+                container.append(oneContainer);
+            }
+            pages++;
+            synch = true;
+            if (!end && getDocumentTop() + getWindowHeight() == getScrollTop()) {
+                synch = false;
+                loadMoreMoments();
+            }
         }
-    })
+    )
 }
-
