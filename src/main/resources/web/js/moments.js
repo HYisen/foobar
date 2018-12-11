c07720386dfb00da1df4284abb87532d3999d373
sbjokergithub <731627938@qq.com>
2018-12-11 15:52:00 +0800

    some adjust

--- src/main/resources/web/js/moments.js
+++ src/main/resources/web/js/moments.js
@@ -23,19 +23,19 @@ function showMoments() {
             body.removeChild(oldContainer)
             body.append(container);
             for (let i in json) {
-                let oneContainer = document.createElement("table");
-                let titleTR = document.createElement("tr");
-                let temp = document.createElement("td");
+                let oneContainer = document.createElement("form");
+                let titleTR = document.createElement("p");
+                let temp = document.createElement("p");
                 temp.innerText = json[i].timestamp;
                 titleTR.append(temp);
-                temp = document.createElement("td");
+                temp = document.createElement("p");
                 temp.innerText = json[i].person.nickname;
                 titleTR.append(temp);
-                temp = document.createElement("td");
+                temp = document.createElement("p");
                 temp.innerText = json[i].paper.title;
                 titleTR.append(temp);
-                let contantTR = document.createElement("tr");
-                temp = document.createElement("td");
+                let contantTR = document.createElement("p");
+                temp = document.createElement("p");
                 temp.innerText = json[i].paper.content;
                 contantTR.append(temp);
                 oneContainer.append(titleTR);
@@ -74,19 +74,19 @@ function loadMoreMoments() {
                 container.appendChild(endContainer);
             }
             for (let i in json) {
-                let oneContainer = document.createElement("table");
-                let titleTR = document.createElement("tr");
-                let temp = document.createElement("td");
+                let oneContainer = document.createElement("form");
+                let titleTR = document.createElement("p");
+                let temp = document.createElement("p");
                 temp.innerText = json[i].timestamp;
                 titleTR.append(temp);
-                temp = document.createElement("td");
+                temp = document.createElement("p");
                 temp.innerText = json[i].person.nickname;
                 titleTR.append(temp);
-                temp = document.createElement("td");
+                temp = document.createElement("p");
                 temp.innerText = json[i].paper.title;
                 titleTR.append(temp);
-                let contantTR = document.createElement("tr");
-                temp = document.createElement("td");
+                let contantTR = document.createElement("p");
+                temp = document.createElement("p");
                 temp.innerText = json[i].paper.content;
                 contantTR.append(temp);
                 oneContainer.append(titleTR);
