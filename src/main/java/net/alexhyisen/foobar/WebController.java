ecd4c586d91f0587c8cd461e4c060e20277ed53b
Alex <alexhyisen@gmail.com>
2018-12-13 22:59:25 +0800

    add the procedure to inject uid into the template

--- src/main/java/net/alexhyisen/foobar/WebController.java
+++ src/main/java/net/alexhyisen/foobar/WebController.java
@@ -1,10 +1,17 @@
 package net.alexhyisen.foobar;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class WebController {
+    @Value("foobar.anonymousUid")
+    String anonymousUid;
+
     @GetMapping(path = "/heaven")
     public String heaven() {
         return "heaven";
@@ -16,7 +23,12 @@ public class WebController {
     }
 
     @GetMapping(path = "/moments")
-    public String moments() {
+    public String moments(Model model, Principal principal) {
+        var name = anonymousUid;
+        if (principal != null) {
+            name = principal.getName();
+        }
+        model.addAttribute("uid", name);
         return "moments";
     }
 
