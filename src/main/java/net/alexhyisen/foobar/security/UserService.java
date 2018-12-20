ef2734693c808c70d651ac49330104c3a3f386d9
Alex <alexhyisen@gmail.com>
2018-12-20 17:21:34 +0800

    replace raw password storage with salted hashed one

--- src/main/java/net/alexhyisen/foobar/security/UserService.java
+++ src/main/java/net/alexhyisen/foobar/security/UserService.java
@@ -1,22 +1,26 @@
 package net.alexhyisen.foobar.security;
 
+import net.alexhyisen.foobar.module.Link;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
-    private LinkRepository linkRepository;
+    private final LinkRepository linkRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(LinkRepository linkRepository) {
+    public UserService(LinkRepository linkRepository, PasswordEncoder passwordEncoder) {
         this.linkRepository = linkRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,5 +28,9 @@ public class UserService implements UserDetailsService {
         final var link = linkRepository.findByAccount_Username(s);
         final var authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
         return new User(link.getPerson().getUid().toString(), link.getAccount().getPassword(), authorities);
+    }
+
+    public Link updatePassword(long uid, String password) {
+        return linkRepository.updatePasswordByUid(uid, passwordEncoder.encode(password));
     }
 }
--- src/main/java/net/alexhyisen/foobar/security/WebSecurityConfig.java
+++ src/main/java/net/alexhyisen/foobar/security/WebSecurityConfig.java
@@ -2,13 +2,15 @@ package net.alexhyisen.foobar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -48,8 +50,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(new UserService(linkRepository))
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-        //TODO: introduce salted encrypted password
+                .userDetailsService(new UserService(linkRepository, passwordEncoder()))
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
