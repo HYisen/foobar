6dbc5f39fb91fcfeb2eaf0af9e234a4421f52505
sbjokergithub <731627938@qq.com>
2018-12-21 21:26:13 +0800

    revert

---
+++ src/main/java/net/alexhyisen/foobar/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package net.alexhyisen.foobar.repository;
+
+import net.alexhyisen.foobar.module.Account;
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends Neo4jRepository<Account, Long> {
+    @Query("MATCH (a:Account{username:{username}}) RETURN count(a)<>0;")
+    boolean existsByUsername(@Param("username") String username);
+}
