104c69276909f642941b3706c769c0f5e02bdcd4
Alex <alexhyisen@gmail.com>
2018-12-26 00:14:56 +0800

    fix the bug that the admin can not access /api/{uid}/**

--- src/main/java/net/alexhyisen/foobar/security/Guard.java
+++ src/main/java/net/alexhyisen/foobar/security/Guard.java
@@ -1,12 +1,18 @@
 package net.alexhyisen.foobar.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Guard {
+    private static final SimpleGrantedAuthority ADMIN_AUTHORITY = new SimpleGrantedAuthority("ROLE_ADMIN");
+
     public boolean check(Authentication authentication, long uidInPath) {
         final String name = authentication.getName();
+        if (authentication.getAuthorities().contains(ADMIN_AUTHORITY)) {
+            return true;
+        }
         if ("anonymousUser".equals(name)) {
             return false;
         }
